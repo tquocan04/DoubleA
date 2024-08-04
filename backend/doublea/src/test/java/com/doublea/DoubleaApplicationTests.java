d0cd544a67a2dda463f64fc9eaa47c0dba4eba09
Ấn Trần <tquocan04@gmail.com>
2024-08-04 22:43:34 +0700

    add backend again

---
+++ backend/doublea/src/test/java/com/doublea/DoubleaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doublea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoubleaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
